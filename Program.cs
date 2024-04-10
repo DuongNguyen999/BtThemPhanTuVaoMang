b94587b3479a25c030796fa425487ade2b8c7e58
DuongNguyen999 <165481404+DuongNguyen999@users.noreply.github.com>
2024-04-11 00:03:50 +0900

    Add du an
    Add du an

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program {
+    public static void Main (string[] args) {
+        
+        int[] array = new int[10];
+
+        Console.Write("Nhap gia tri chen (X): ");
+        int X = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhap vi tri chen X : ");
+        int index = int.Parse(Console.ReadLine());
+
+        if (index <= 1 || index >= array.Length - 1) 
+        {
+            Console.WriteLine("Khong chen");
+        } 
+        else 
+        {
+            for (int i = 1; i < array.Length-1; i++) 
+            {
+                array[i] = array[i + 1];
+            }
+            array[index] = X;
+
+            
+            Console.WriteLine("Mang sau khi chen X:");
+            
+            Console.WriteLine(string.Join(",", array));
+            
+        }
+    }
+}
